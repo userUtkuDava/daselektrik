fff06daa82dff1c635256048dcea34dab1f68b79
userUtkuDava <utkudava.js@gmail.com>
2024-10-25 11:21:55 +0300

    linkler düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import './App.css';
 import Menu from './Components/nav/Menu';
 import İnfoHead from './Components/info-head/İnfo';
 import İletisim from './Components/pages/iletişim/İletişim';
-import Hizmleter from './Components/pages/hizmetler/Hizmetler';
+import Hizmetler from './Components/pages/hizmetler/Hizmetler';
 import Blog from './Components/pages/blog/Blog';
 import Hakkımızda from './Components/pages/hakkımızda/Hakkımızda';
 import Katalog from './Components/pages/katalog/Katalog';
 import NotFound from './Components/notfound/NotFound';
 import Home from './Components/anasayfa/Home';
 import BlogDetail from './Components/pages/blog/BlogDetail';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import UpButton from './Components/anasayfa/upbutton/UpButton';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Footer from './Components/footer/Footer';
 
 function App() {
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -23,28 +22,23 @@ function App() {
   return (
     <>
       <İnfoHead />
-
       <nav>
         <Menu />
-
-        <Routes>
-          <Route path='/daselektrik' element={<Home />}></Route>
-          <Route path='/daselektrik/katalog' element={<Katalog />}></Route>
-          <Route path='/daselektrik/blog/:id' element={<BlogDetail />}></Route>
-          <Route path='/daselektrik/İletisim' element={<İletisim />}></Route>
-          <Route path='*' element={<NotFound />}></Route>
-          <Route path='/daselektrik/hizmetler' element={<Hizmleter />}></Route>
-          <Route path='/daselektrik/blog' element={<Blog />}></Route>
-          <Route path='/daselektrik/hakkımızda' element={<Hakkımızda />}></Route>
-        </Routes>
       </nav>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/katalog" element={<Katalog />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+        <Route path="/iletisim" element={<İletisim />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/hizmetler" element={<Hizmetler />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/hakkımızda" element={<Hakkımızda />} />
+      </Routes>
       <UpButton />
       <Footer />
     </>
   );
 }
-
-
 
 export default App;
--- src/Components/nav/Menu.jsx
+++ src/Components/nav/Menu.jsx
@@ -59,12 +59,12 @@ const Menu = () => {
                 <div >
 
                     <ul id='navbar' >
-                        <Link to={"/daselektrik"}><li><MdHome className='nav-ico' /><a href='#' className='active'>Ana sayfa</a ></li></Link>
-                        <Link to={"daselektrik/katalog"}><li><AiFillProduct className='nav-ico' /><a href='#'>Katalog</a></li></Link>
-                        <Link to={"daselektrik/blog"}> <li><ImBlog className='nav-ico' /><a href='#'>Blog</a></li></Link>
-                        <Link to={"daselektrik/hakkımızda"}><li><GoCrossReference className='nav-ico' /><a href='#'>Hakkımızda</a></li></Link>
-                        <Link to={"daselektrik/İletisim"}><li><RiCustomerService2Fill className='nav-ico' /><a href='#'>İletişim</a></li></Link>
-                        <Link to={"daselektrik/hizmetler"}><li><RiServiceLine className='nav-ico' /><a href='#'>Hizmetlerimiz</a></li></Link>
+                        <Link to={"/"}><li><MdHome className='nav-ico' /><a href='#' className='active'>Ana sayfa</a ></li></Link>
+                        <Link to={"/katalog"}><li><AiFillProduct className='nav-ico' /><a href='#'>Katalog</a></li></Link>
+                        <Link to={"/blog"}> <li><ImBlog className='nav-ico' /><a href='#'>Blog</a></li></Link>
+                        <Link to={"/hakkımızda"}><li><GoCrossReference className='nav-ico' /><a href='#'>Hakkımızda</a></li></Link>
+                        <Link to={"/iletisim"}><li><RiCustomerService2Fill className='nav-ico' /><a href='#'>İletişim</a></li></Link>
+                        <Link to={"/hizmetler"}><li><RiServiceLine className='nav-ico' /><a href='#'>Hizmetlerimiz</a></li></Link>
                     </ul>
                 </div>
 
